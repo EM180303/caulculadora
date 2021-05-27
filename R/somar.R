39ca0e83adefc5b13214c1e244d8200e9ced32f2
EM180303 <e.marques180303@gmail.com>
2021-05-27 12:40:53 -0300

    First comit

---
+++ R/somar.R
@@ -0,0 +1,20 @@
+#'Função para somar
+#'
+#'Soma 2 números
+#'
+#'@param a um número
+#'@param b outro número
+#'
+#'@examples
+#'somar(3,4)
+#'
+#'@export
+somar <- function(a, b)
+{
+  a + b
+}
+
+subitrair <- function(a, b)
+{
+  a - b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,15 @@
+#'Função para subtrair
+#'
+#'Subitrai 2 números
+#'
+#'@param a um número
+#'@param b outro número
+#'
+#'@examples
+#'subtrair(3,4)
+#'
+#'@export
+subitrair <- function(a, b)
+{
+  a - b
+}
